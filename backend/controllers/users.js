27af21f078205d3f6b61ea80db23acc05e398518
Amory <garbage90@yandex.ru>
2021-01-16 16:56:18 +0300

    token expiresIn

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -131,7 +131,11 @@ const login = (req, res, next) => {
 
   return userModel.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
       res.send({ token });
     })
     .catch((err) => {
