3d5893226fb4f438502787d1f7a10857ea1323d5
Amory <garbage90@yandex.ru>
2021-01-28 22:20:23 +0300

    second review bug fix

--- backend/middleware/validateUsers.js
+++ backend/middleware/validateUsers.js
@@ -22,7 +22,7 @@ const checkUpdateAvatar = celebrate({
 const checkLogin = celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email(),
-    password: Joi.string().required().min(8).max(30),
+    password: Joi.string().required(),
   }),
 });
 
